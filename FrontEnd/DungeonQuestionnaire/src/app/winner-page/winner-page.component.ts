fd4dbf2ae32148c115090f2d1c41c4ebc8d5b139
Roomsie Jones <seangravito@gmail.com>
2022-03-07 14:37:13 -0600

    setup angular project and added some base components we will need

---
+++ FrontEnd/DungeonQuestionnaire/src/app/winner-page/winner-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-winner-page',
+  templateUrl: './winner-page.component.html',
+  styleUrls: ['./winner-page.component.sass']
+})
+export class WinnerPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

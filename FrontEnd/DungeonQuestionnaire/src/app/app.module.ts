533bf4e312c3c4d1caf190288af39a27fa7fef62
Kristopher Wasserman <kristopherwasserman6@msn.com>
2022-03-09 16:27:00 -0600

    added new nav and footer

--- FrontEnd/DungeonQuestionnaire/src/app/app.module.ts
+++ FrontEnd/DungeonQuestionnaire/src/app/app.module.ts
@@ -10,12 +10,9 @@ import { WinnerPageComponent } from './winner-page/winner-page.component';
 import { GameOverComponent } from './game-over/game-over.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
-<<<<<<< HEAD
-=======
 import { UpgradetextComponent } from './upgradetext/upgradetext.component';
 import { NewSpriteChoiceComponent } from './new-sprite-choice/new-sprite-choice.component';
 import { PlayAgainComponent } from './play-again/play-again.component';
->>>>>>> d3e290c36dbd1b8871663edfc2bcdc0096ff2957
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
@@ -35,11 +32,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
-    FontAwesomeModule
-=======
     FontAwesomeModule,
->>>>>>> d3e290c36dbd1b8871663edfc2bcdc0096ff2957
   ],
   providers: [],
   bootstrap: [AppComponent]

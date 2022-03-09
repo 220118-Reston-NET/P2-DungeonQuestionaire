2f6f11ddd266a8439c43451fc16fe74ac4857267
Chad Solomon <chadcsoloman@gmail.com>
2022-03-08 20:39:49 -0500

    added repositorys

--- BackEnd/Api/Program.cs
+++ BackEnd/Api/Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IRepository>(repo => new PlayerRepository(builder.Configuration.GetConnectionString("Reference2DB")));
+builder.Services.AddScoped<IRepository>(repo => new EnemyRepository(builder.Configuration.GetConnectionString("Reference2DB")));
+builder.Services.AddScoped<IRepository>(repo => new QuestionRepository(builder.Configuration.GetConnectionString("Reference2DB")));
 
 var app = builder.Build();
 

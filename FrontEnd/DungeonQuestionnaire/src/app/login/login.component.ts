cad92b0be783b4350050d5a87db479a4b8ed7aec
Kristopher Wasserman <kristopherwasserman6@msn.com>
2022-03-10 17:01:51 -0600

    new login

--- FrontEnd/DungeonQuestionnaire/src/app/login/login.component.ts
+++ FrontEnd/DungeonQuestionnaire/src/app/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  menuLabel = "";
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  goToSignUp()
+  {
+    this.router.navigate(["/signup"]);
+    this.menuLabel = "Sign up(Main Menu)";
+  }
 }

e8a690ed4fb05dd183d1177c345d1b63f71cb28c
Roomsie Jones <seangravito@gmail.com>
2022-03-09 21:06:01 -0600

    added save-logout page

--- FrontEnd/DungeonQuestionnaire/src/app/save-logout/save-logout.component.ts
+++ FrontEnd/DungeonQuestionnaire/src/app/save-logout/save-logout.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-save-logout',
   templateUrl: './save-logout.component.html',
-  styleUrls: ['./save-logout.component.sass']
+  styleUrls: ['./save-logout.component.css']
 })
 export class SaveLogoutComponent implements OnInit {
 

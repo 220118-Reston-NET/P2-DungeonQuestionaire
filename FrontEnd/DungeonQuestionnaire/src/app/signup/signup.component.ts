1678d5ccd5b2b2a6c8c6fe54faaa94f5437c92c9
Kristopher Wasserman <kristopherwasserman6@msn.com>
2022-03-11 21:47:38 -0600

    made sigup work. fix some issues in nav-bar

--- FrontEnd/DungeonQuestionnaire/src/app/signup/signup.component.ts
+++ FrontEnd/DungeonQuestionnaire/src/app/signup/signup.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.sass']
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
+  menuLabel = "";
   ngOnInit(): void {
   }
-
+  goToLogin()
+  {
+    this.router.navigate(["/login"]);
+    this.menuLabel = "Sign up(Main Menu)";
+  }
 }

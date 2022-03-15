d9c8ac28b2681289886db3bcd2311807d3124911
Roomsie Jones <seangravito@gmail.com>
2022-03-14 21:20:29 -0500

    Fixed question bug by having a 5th element in the answers and having it be hidden, and setting the ngModels to set it to that

--- FrontEnd/DungeonQuestionnaire/src/app/questions-answers/questions-answers.component.ts
+++ FrontEnd/DungeonQuestionnaire/src/app/questions-answers/questions-answers.component.ts
@@ -10,10 +10,10 @@ import { Router } from '@angular/router';
 })
 export class QuestionsAnswersComponent implements OnInit {
     question:string = "";
-    answer1:string = "";
-    answer2:string = "";
-    answer3:string = "";
-    answer4:string = "";
+    answer1:string = "loading";
+    answer2:string = "loading.";
+    answer3:string = "loading..";
+    answer4:string = "loading...";
     correctAnswer:string = "Correct Answer ";
     questionAttack:number = 0;
     listOfQuestion:Question[];
@@ -24,10 +24,11 @@ export class QuestionsAnswersComponent implements OnInit {
     enemyAttack: number = 0;
     correct: string | null = ""
     userVictories:number = 0;
+    hidden = ""
 
 
     @Input()
-    answer = "";
+    answer = this.hidden;
 
     @Output() 
     playerHPEmitter = new EventEmitter<number>();
@@ -163,6 +164,7 @@ export class QuestionsAnswersComponent implements OnInit {
 
       }
       this.changeQuestionsAndAnswers(0);
+      this.answer = this.hidden;
      
 
 
